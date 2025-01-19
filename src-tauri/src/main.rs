2422212a17ed1d1bec6cb5a48aa7855b51d026fa
fantasyzhjk <fantasyzhjk@outlook.com>
2025-01-19 19:00:44 +0800

    fix

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -401,7 +401,6 @@ fn main() -> AnyResult<()> {
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
             println!("{}, {argv:?}, {cwd}", app.package_info().name);
         }))
